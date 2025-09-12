1714c0fdcc36b1fb497456434abe86c93a1d5966
alexanderley <112874444+alexanderley@users.noreply.github.com>
2025-09-12 14:35:45 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Trains from "../src/components/Trains/Trains";
+
+function App() {
+  return (
+    <>
+      <h1>Trains</h1>
+      <Trains />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Trains/Trains.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import styles from "./Train.module.scss";
+
+const Trains: React.FC = () => {
+  return <div className={styles.trainWrapper}></div>;
+};
+
+export default Trains;
