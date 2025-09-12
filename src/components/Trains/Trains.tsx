5eb0df545b9c155e04a2875e8ecae44ce741234a
alexanderley <112874444+alexanderley@users.noreply.github.com>
2025-09-12 21:59:56 +0200

    fixed typo in import

--- src/components/Trains/Trains.tsx
+++ src/components/Trains/Trains.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import styles from "./Train.module.scss";
 import Arrivals from "../Arrivals/Arrivals";
 import Departures from "../Departures/Departures";
-import SearchStaion from "../SearchStation/SearchStaion";
+import SearchStation from "../SearchStation/SearchStation";
 
 const Trains: React.FC = () => {
   return (
     <div className={styles.trainWrapper}>
-      <SearchStaion />
+      <SearchStation />
       <Arrivals />
       <Departures />
     </div>
